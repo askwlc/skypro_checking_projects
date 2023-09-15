e957e426779eeb77e58a7e554716846fc5d38c31
Никита Шинков <shinkovn@yandex.ru>
2023-09-15 20:53:45 +0400

    models, registartion done

--- skypro_checking_projects/apps/accounts/views.py
+++ skypro_checking_projects/apps/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def registration(request: HttpRequest) -> HttpResponse:
+    """Обработка регистрации пользователя."""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('files_list')
+        else:
+            form = UserCreationForm()
+        return render(request, 'registration.html', {'form': form})
--- skypro_checking_projects/apps/files/models.py
+++ skypro_checking_projects/apps/files/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class FileUpload(models.Model):
+    """Модель загруженного файла, с полями датами загрузки и проверки, было ли редактирование."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='files/')
+    upload_time = models.DateTimeField(auto_now_add=True)
+    is_new = models.BooleanField(default=True)
+    check_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.file.name
+
+
+class FileCheckLogs(models.Model):
+    """Модель хранения логов проверок."""
+    STATUS_CHOICES = [
+        ('waiting', 'Ожидание'),
+        ('completed', 'Завершено'),
+        ('error', 'Ошибка')
+    ]
+
+    file = models.ForeignKey(FileUpload, on_delete=models.CASCADE, related_name='logs')
+    check_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS_CHOICES, default='waiting')
+    result = models.TextField(null=True, blank=True)
+    notification_sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.file
